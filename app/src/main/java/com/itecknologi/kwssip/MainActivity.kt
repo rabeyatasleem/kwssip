757d26b3ea457b2195e67e414def4a7859fefb15
rabeya.tasleem <rabeyatasleem1@gmail.com>
2024-07-03 17:04:46 +0500

    SplashScreen Login HomeScreen

---
+++ app/src/main/java/com/itecknologi/kwssip/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.itecknologi.kwssip
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.itecknologi.kwssip.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.imageView1.setOnClickListener{
+            openCamera()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val imageUri = data?.data
+        if (imageUri != null) {
+            // Display the captured image
+            displayCapturedImage(imageUri)
+        }
+    }
+
+    private fun displayCapturedImage(imageUri: Uri) {
+        binding.imageView1.setImageURI(imageUri)
+    }
+
+    // Open the camera using an Intent
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            return
+        }
+    }
+}
---
+++ app/src/main/java/com/itecknologi/kwssip/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.itecknologi.kwssip
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+
+        }
+    }
+}
